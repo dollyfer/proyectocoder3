077c088a7821de8ab3352bc02a0195278ff28eb9
Dolly Fernandez <dollyfernandezrc@gmail.com>
2022-06-02 18:14:38 -0300

    use fetch

--- javascript/main.js
+++ javascript/main.js
@@ -10,26 +10,28 @@ Swal.fire({
 
 
 
-function cargarProductos(){
-    let productos2= cargarProductosLS();
-    let contenido= "";
 
-  for (const producto of productos2) {
-    contenido += `
-  <div class="card" style="width: 18rem;">
-  <img src="../imagenes/${producto.imag}" class="card-img-top" alt="${producto.insumos}">
-  <div class="card-body">
-    <h5 class="card-title">${producto.insumos}</h5>
-    <p class="card-text">$${producto.precio}</p>
-    <a href="#" class="btn btn-primary" onclick="agregarAlCarrito(${producto.id});">Adquirir</a>
-  </div>
- </div> `;
-    }
 
-    document.getElementById("productos").innerHTML = contenido;
-}
+const resultado = document.getElementById("productos");
+fetch('/javascript/productos.json')
+.then((response) => response.json())
+.then ((data)=> 
+data.forEach(productos=> {
+    
+    let nuevo = document.createElement("div");
+    nuevo.classList.add("card");
+    nuevo.innerHTML =
+    `<img src= "${productos.imag}" class="card-img-top">
+    <div class = "card-body ">
+    <h5 class= card-title>  ${productos.insumos}</h5>
+    <p class= card-text>$${productos.precio}</p>
+    <a  class="btn btn-primary" onclick="agregarAlCarrito(${productos.id});">COMPRAR</a>`;
+    resultado.appendChild(nuevo);
+    
+    
+}))
 guardarProductosLS(productos);
-cargarProductos();
+
 actualizarBotonCarrito();
 
 
