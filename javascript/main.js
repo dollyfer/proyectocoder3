144a193e8031513bdd54f8ad9a1f35c7ccb5ed03
Dolly Fernandez <dollyfernandezrc@gmail.com>
2022-06-16 14:20:39 -0400

    nuevo commit

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,3 @@
-
 Swal.fire({
   title: 'Bienvenido a la tienda de Nails With Do',
   text: 'Los mejores productos y precios',
@@ -9,10 +8,24 @@ Swal.fire({
 })
 
 
-
-
-
 const resultado = document.getElementById("productos");
+
+const creacionProductos = () => {
+  listaProductos.forEach((product) => {
+  let nuevo = document.createElement("div");
+    nuevo.classList.add("card");
+    nuevo.innerHTML =
+    `<img src= "${product.imag}" class="card-img-top">
+    <div class = "card-body ">
+    <h5 class= card-title>  ${product.insumos}</h5>
+    <p class= card-text>$${product.precio}</p>
+    <a  class="btn btn-primary" onclick="agregarAlCarrito(${product.id});">COMPRAR</a>`;
+    resultado.appendChild(nuevo); 
+ });
+}
+
+/*
+
 fetch('/javascript/productos.json')
 .then((response) => response.json())
 .then ((data)=> 
@@ -30,9 +43,12 @@ data.forEach(productos=> {
     
     
 }))
-guardarProductosLS(productos);
 
+*/
+guardarProductosLS(listaProductos);
 actualizarBotonCarrito();
+creacionProductos();
+
 
 
 
--- javascript/productos.js
+++ javascript/productos.js
@@ -1,19 +1,46 @@
-const productos =[
-    {id: 1, insumos: "Ultrabond Cherimoya 15ml", precio: 800,  imag: "../imagenes/ultrabond cherimoya.jpg"},
-    { id: 2, insumos: "Lima Cherimoya 100/180", precio: 300,  imag: "../imagenes/lima cherimoya.png"},
-    { id: 3, insumos: "Gel color OPI", precio: 950, imag: "../imagenes/opicolor3.png"},
-    { id: 4, insumos: "Gel color OPI", precio: 950,  imag: "../imagenes/opicolor2.png"},
-    { id: 5, insumos: "Gel color OPI", precio: 950, imag: "../imagenes/opicolor1.png"},
-    { id: 6, insumos: "Top Coat Cherimoya 15ml", precio: 1150,  imag: "../imagenes/cherimoya top.webp"},
-    { id: 7, insumos: "Nail prep Nails Show 10ml", precio: 650, imag: "../imagenes/nails_prep.png"},
-    { id: 8, insumos: "Base Coat Cherimoya 15ml", precio: 1100,  imag: "../imagenes/cherimoya base.jpg"},
-    { id: 9, insumos: "Buffer OPI", precio: 250, imag: "../imagenes/buffer opi.jpg" }
-];
+class productosSeleccionado {
+    constructor (id, insumos, precio, imag){
+        this.id = id;
+        this.insumos = insumos;
+        this.precios = precio;
+        this.imag = imag;
+    }
+}
 
-function guardarProductosLS(productos){
-    localStorage.setItem("productos", JSON.stringify(productos));
+let listaProductos = [];
+
+const productos = window.localStorage.getItem(`productos_seleccionados`);
+
+if (productos){
+    listaProductos = JSON.parse(window.localStorage.getItem(`productos_seleccionados`))
+} else {
+    listaProductos.push(new productosSeleccionado(1, "pepe",300, "../imagenes/ultrabond cherimoya.jpg")),
+    listaProductos.push(new productosSeleccionado(2, "perez",350, "../imagenes/ultrabond cherimoya.jpg")),
+    listaProductos.push(new productosSeleccionado(3, "agus",300, "../imagenes/ultrabond cherimoya.jpg")),
+    listaProductos.push(new productosSeleccionado(4, "Ultrabond Cherimoya 15ml",300, "../imagenes/ultrabond cherimoya.jpg")),
+    listaProductos.push(new productosSeleccionado(5, "Ultrabond Cherimoya 15ml",300, "../imagenes/ultrabond cherimoya.jpg")),
+    listaProductos.push(new productosSeleccionado(6, "Ultrabond Cherimoya 15ml",300, "../imagenes/ultrabond cherimoya.jpg")),
+    listaProductos.push(new productosSeleccionado(7, "Ultrabond Cherimoya 15ml",300, "../imagenes/ultrabond cherimoya.jpg")),
+    listaProductos.push(new productosSeleccionado(8, "Ultrabond Cherimoya 15ml",300, "../imagenes/ultrabond cherimoya.jpg")),
+    listaProductos.push(new productosSeleccionado(9, "Ultrabond Cherimoya 15ml",300, "../imagenes/ultrabond cherimoya.jpg"))
+}
+
+// const productos =[
+//     {id: 1, insumos: "Ultrabond Cherimoya 15ml", precio: 800,  imag: "../imagenes/ultrabond cherimoya.jpg"},
+//     { id: 2, insumos: "Lima Cherimoya 100/180", precio: 300,  imag: "../imagenes/lima cherimoya.png"},
+//     { id: 3, insumos: "Gel color OPI", precio: 950, imag: "../imagenes/opicolor3.png"},
+//     { id: 4, insumos: "Gel color OPI", precio: 950,  imag: "../imagenes/opicolor2.png"},
+//     { id: 5, insumos: "Gel color OPI", precio: 950, imag: "../imagenes/opicolor1.png"},
+//     { id: 6, insumos: "Top Coat Cherimoya 15ml", precio: 1150,  imag: "../imagenes/cherimoya top.webp"},
+//     { id: 7, insumos: "Nail prep Nails Show 10ml", precio: 650, imag: "../imagenes/nails_prep.png"},
+//     { id: 8, insumos: "Base Coat Cherimoya 15ml", precio: 1100,  imag: "../imagenes/cherimoya base.jpg"},
+//     { id: 9, insumos: "Buffer OPI", precio: 250, imag: "../imagenes/buffer opi.jpg" }
+// ];
+
+function guardarProductosLS(listaProductos){
+    localStorage.setItem("productos_seleccionados", JSON.stringify(listaProductos));
 }
 
 function cargarProductosLS(){
-    return JSON.parse(localStorage.getItem("productos"));
+    return JSON.parse(localStorage.getItem("productos_seleccionados"));
 }
